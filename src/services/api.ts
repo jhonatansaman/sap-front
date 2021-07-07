47a51940bbcdb659330a93580d72711fc2058b8f
jhonatan <jhonatan.saman@gok.digital>
2021-07-06 22:34:29 -0300

    fix: env api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const url =
   process.env.NODE_ENV === 'production'
-    ? process.env.API_URL
-    : process.env.API_LOCAL;
+    ? process.env.REACT_APP_API_URL
+    : process.env.REACT_APP_API_LOCAL;
 
 const api = axios.create({
   baseURL: url,
