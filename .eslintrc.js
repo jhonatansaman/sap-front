5d158df0fea19f9df72b6191cd1b05b3a93bcf08
jhonatan <jhonatan.saman@gok.digital>
2021-07-07 20:41:57 -0300

    fix: eslint on windows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,80 @@
 // eslint-disable-next-line no-undef
-module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: "module",
-    },
-    plugins: ["react"],
-    rules: {
-          "no-console": "warn",
-          "import/first": "error",
-      "react/prop-types": 0,
-      "linebreak-style": ["error", "unix"]
-    },
-    extends: ["prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "linebreak-style": ["error", "unix"],
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off",
-  },
-}
 
-  
+
+module.exports = {
+
+  env: {
+
+    browser: true,
+
+
+    es2021: true,
+
+  },
+
+
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+
+
+  parserOptions: {
+
+    ecmaFeatures: {
+
+      jsx: true,
+
+    },
+
+
+    ecmaVersion: 12,
+
+
+    sourceType: 'module',
+
+  },
+
+
+  plugins: ['react'],
+
+
+  rules: {
+
+    'no-console': 'warn',
+
+
+    'import/first': 'error',
+
+
+    'react/prop-types': 0,
+
+
+    'linebreak-style': ['error', 'unix'],
+
+  },
+
+
+  extends: ['prettier'],
+
+
+  plugins: ['prettier'],
+
+
+  rules: {
+
+    'prettier/prettier': 'error',
+
+
+    'linebreak-style': ['error', 'unix'],
+
+
+    'arrow-body-style': 'off',
+
+
+    'prefer-arrow-callback': 'off',
+
+
+    'linebreak-style': ['error', 'windows'],
+
+  },
+
+};
+
