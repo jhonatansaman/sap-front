bab0d8ae992b0cb6f04b59ae12737cd568395129
jhonatan <jhonatan.saman@gok.digital>
2021-09-03 17:28:49 -0300

    refactor: routes

---
+++ src/routes/cadasterRoutes.tsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import {Switch, Route, useRouteMatch} from 'react-router-dom';
+import Sidebar from '../assets/components/sidebar';
+import {MenuSidebar, StateRoles} from '../assets/components/sidebar/index.type';
+import {userService} from '../services/user';
+import IconCollegiate from '../assets/UIkit/icons/ico-collegiate.svg';
+import IconCollegiateActived from '../assets/UIkit/icons/ico-collegiate_actived.svg';
+import CadasterContainer from '../screens/containers/cadaster';
+
+const menus: Array<MenuSidebar> = [
+  {
+    label: 'Colegiado',
+    subRoutes: [{name: 'Cadastrar Plano'}],
+    icon: IconCollegiate,
+    iconActived: IconCollegiateActived,
+    isActived: false,
+  },
+  {
+    label: 'Eventos',
+    subRoutes: [{name: 'Cadastrar Feriados'}],
+    icon: IconCollegiate,
+    iconActived: IconCollegiateActived,
+    isActived: false,
+  },
+];
+
+const {user}: any = userService.getUserInfo();
+
+const CadasterRoutes: React.FC = () => {
+  const [roles] = useState<StateRoles>({data: userService.getRoles()});
+  const [routes, setRoutes] = React.useState<Array<MenuSidebar>>(menus);
+  const path = useRouteMatch();
+  console.log(path);
+
+  return (
+    <React.Fragment>
+      <Sidebar
+        user={user}
+        data={roles.data}
+        routes={routes}
+        onClickMenu={(param: number) =>
+          setRoutes(
+            routes.map((item, index) =>
+              index === param ? {...item, isActived: !item.isActived} : item,
+            ),
+          )
+        }
+      />
+      <Switch>
+        <Route exact path={`${path.path}/colegiado`}>
+          <CadasterContainer routes={menus} />
+        </Route>
+      </Switch>
+    </React.Fragment>
+  );
+};
+
+export default CadasterRoutes;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,32 +8,11 @@ import CadasterContainer from '../screens/containers/cadaster';
 import {userService} from '../services/user';
 import Sidebar from '../assets/components/sidebar';
 import {MenuSidebar, StateRoles} from '../assets/components/sidebar/index.type';
-import IconCollegiate from '../assets/UIkit/icons/ico-collegiate.svg';
-import IconCollegiateActived from '../assets/UIkit/icons/ico-collegiate_actived.svg';
-import {Container} from '../screens/components/cadaster/styles';
-const {user}: any = userService.getUserInfo();
 
-const menus: Array<MenuSidebar> = [
-  {
-    label: 'Colegiado',
-    subRoutes: [{name: 'Cadastrar Plano'}],
-    icon: IconCollegiate,
-    iconActived: IconCollegiateActived,
-    isActived: false,
-  },
-  {
-    label: 'Eventos',
-    subRoutes: [{name: 'Cadastrar Feriados'}],
-    icon: IconCollegiate,
-    iconActived: IconCollegiateActived,
-    isActived: false,
-  },
-];
+import {Container} from '../screens/components/cadaster/styles';
+import CadasterRoutes from './cadasterRoutes';
 
 const Routes: React.FC = () => {
-  const [roles] = useState<StateRoles>({data: userService.getRoles()});
-  const [routes, setRoutes] = React.useState<Array<MenuSidebar>>(menus);
-
   return (
     <>
       <Router>
@@ -48,23 +27,7 @@ const Routes: React.FC = () => {
             <Home />
           </Route>
           <Route path="/cadaster">
-            <Container>
-              <Sidebar
-                user={user}
-                data={roles.data}
-                routes={routes}
-                onClickMenu={(param: number) =>
-                  setRoutes(
-                    routes.map((item, index) =>
-                      index === param
-                        ? {...item, isActived: !item.isActived}
-                        : item,
-                    ),
-                  )
-                }
-              />
-              <CadasterContainer routes={menus} />
-            </Container>
+            <CadasterRoutes />
           </Route>
         </Switch>
       </Router>
